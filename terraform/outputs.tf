5b188afb5da0570f51418c7f0c3cb3d73ab676a2
Chinmay Jog <chinmaymjog@gmail.com>
2025-08-02 11:18:09 +0530

    hosts file output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,16 +13,30 @@ Bastion VM Private IP = ${module.hub.vm_private_ip}
 Production Resource Group Name = ${module.prod-web.resource_group_name}
 Production MySQL server Name = ${module.prod-web.mysql_name}
 Production Load Balancer IP = ${module.prod-web.lb_ip}
-Production Web Server Private IPs
-${module.prod-web.web_vms[0]} = ${module.prod-web.web_vms_private_ips[0]}
-${module.prod-web.web_vms[1]} = ${module.prod-web.web_vms_private_ips[1]}
+  Production Web Server Private IPs
+  ${module.prod-web.web_vms[0]} = ${module.prod-web.web_vms_private_ips[0]}
+  ${module.prod-web.web_vms[1]} = ${module.prod-web.web_vms_private_ips[1]}
 
 PreProduction Resource Group Name = ${module.preprod-web.resource_group_name}
 PreProduction MySQL server Name = ${module.preprod-web.mysql_name}
 PreProduction Load Balancer IP = ${module.preprod-web.lb_ip}
-PreProduction Web Server Private IPs
-${module.preprod-web.web_vms[0]} = ${module.preprod-web.web_vms_private_ips[0]}
-${module.preprod-web.web_vms[1]} = ${module.preprod-web.web_vms_private_ips[1]}
+  PreProduction Web Server Private IPs
+  ${module.preprod-web.web_vms[0]} = ${module.preprod-web.web_vms_private_ips[0]}
+  ${module.preprod-web.web_vms[1]} = ${module.preprod-web.web_vms_private_ips[1]}
 
 CUSTOM_OUTPUT  
+}
+
+resource "local_file" "hosts" {
+  content  = <<EOF
+[production]
+${module.prod-web.web_vms[0]} ansible_host=${module.prod-web.web_vms_private_ips[0]}
+${module.prod-web.web_vms[1]} ansible_host=${module.prod-web.web_vms_private_ips[1]}
+
+[preproduction]
+${module.preprod-web.web_vms[0]} ansible_host=${module.preprod-web.web_vms_private_ips[0]}
+${module.preprod-web.web_vms[1]} ansible_host=${module.preprod-web.web_vms_private_ips[1]}  
+
+EOF
+  filename = "${path.module}/hosts"
 }
